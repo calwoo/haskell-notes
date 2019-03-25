18d8ab67a38b865c946268349befbe22fccf631a
Calvin D. Woo <calvin.d.woo@gmail.com>
2019-03-25 12:28:27 -0400

    interp

--- fprob-prog/simple-pp.hs
+++ fprob-prog/simple-pp.hs
@@ -1,2 +1,7 @@
 -- really basic probabilistic programming interpreter
 
+import Data.List hiding (empty, insert, map)
+import Control.Monad
+import System.Random.MWC as MWC
+import System.Random.MWC.Distributions as MD
+
